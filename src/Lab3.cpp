550d351f07d3e1c9bfcd8e3dccf2eeee58b120d5
DoBeJamin <benjaminick11@gmail.com>
2023-06-27 18:30:01 -0500

    Final Commit

--- src/Lab3.cpp
+++ src/Lab3.cpp
@@ -13,15 +13,17 @@ SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
 int color[3] = {255,255,255};
+
+//interal varialbes
+bool button_pressed = false;
+int potentiometer;
 int color_idx = 0;
 
-bool button_pressed = false;
 
-int potentiometer;
-
-using namespace std;
 
 void setup() {
+
+  //initialize pins
   pinMode(D7, OUTPUT);
   pinMode(D6, OUTPUT);
   pinMode(D3, OUTPUT);
@@ -29,13 +31,11 @@ void setup() {
   pinMode(D8, INPUT);
   pinMode(A5, INPUT);
 
-
 }
-
 
 void loop() {
 
-
+  //changes what color is being updated when the button is pressed
   if (digitalRead(D8)) {
     button_pressed = true;
   } else if (button_pressed) {
@@ -44,20 +44,14 @@ void loop() {
       color_idx = 0;
     }
     button_pressed = false;
-    
-
   }
 
+  // updates current color intensity
   potentiometer = analogRead(A5);
-
-
   color[color_idx] = static_cast<float>(potentiometer)*(255.0/4095.0);
-
-
 
   analogWrite(D7,color[0]); 
   analogWrite(D6,color[1]); 
   analogWrite(D3,color[2]); 
-
 
 }
